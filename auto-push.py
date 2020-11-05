a3242ef79621f0bdf6bc0bad7a3fc9ca9765e3f7
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2020-11-04 23:31:31 -0600

    auto push add select branch

--- auto-push.py
+++ auto-push.py
@@ -1,6 +1,7 @@
 import os
-message = input('What did you do, dude? 🙃🎤 :')
+message = input('What did you do?, dude 🙃🎤 :')
+branch = input('What branch? DUDE 🤨:')
 os.system('git add .')
 os.system(f'git commit -m "{message}"')
-os.system('git push origin master')
+os.system(f'git push origin {branch}')
 os.system('echo Ready!!! Dude 😎')
