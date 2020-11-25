703c291bf16127c2d6889ef1955fc86857b5bca8
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2020-11-25 05:05:34 -0600

    agregando archivos a laseccion 7

--- Sesion_7/reto1.sql
+++ Sesion_7/reto1.sql
@@ -1,18 +1,3 @@
--- crear db --
-create database if not exists etesech;
--- borrar db --
-drop database if exists etesech;
-use etesech;
--- crear tabla users --
-create table if not exists users(
-	user_id int primary key,
-    gender char(1),
-    age int,
-    occupation int,
-    zipcode varchar(20)	
-);
--- borrar tabla --
-drop table if exists users;
 
 -- crear tabla movies --
 create table if not exists movies(
@@ -29,11 +14,5 @@ create table if not exists ratings(
 	movie_id int,
 	rating int,
 	rated_at bigint
-    -- ,
-    -- foreign key (user_id) references users(user_id),
-	-- foreign key (movie_id) references movies(user_id)
 );
 drop table if exists ratings;
-select * from users;
-select * from ratings;
-select * from movies;
