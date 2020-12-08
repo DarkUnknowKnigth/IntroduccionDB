d7c033ef0df92800dc8a7f63c2ae02ed059d05ed
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2020-12-08 17:45:43 -0600

    actualizacion a plot de js

--- anomalies/analisis.js
+++ anomalies/analisis.js
@@ -11,26 +11,28 @@ fs.createReadStream('mexico.csv')
   })
   .on('end', () => {
     console.log('> importados');
-    // const m = [{x: calcDistance(mexico).map( r => r[0]), y: calcDistance(mexico).map(r => r[1]), type: 'scatter'}];
-    // plot(data);
-    m = calcDistance(mexico);
+    let m = calcDistance(mexico);
     const csv = exportCSV.parse(m);
     fs.writeFile('distanceMexico.csv',csv, function (err) {
       if (err) return console.log(err);
       console.log('csv > mexicoWithDistance');
     });
+    data = [{x: m.map( r => r.lat), y: m.map(r => r.rv), type: 'scatter'}];
+    plot(data);
   });
 function calcDistance(mexico){
-  let mexicoFixing =  mexico
+  let mexicoFixing =  mexico //Ordenar los datos
   .sort( (l1,l2) => { 
     return (l1.lat+l1.long < l2.lat+l2.long) ? -1 : 1;
   })
-  .map((location, index, element) =>{
+  .map((location, index, element) =>{ //agregar los vecinos cercanos deacuerdo al orden
     return { location:location, v1:element[index+1], v2: element[index+2]};
   });
+  //arreglar el index -1 y -2 null de los ultimos en el arreglo
   mexicoFixing[mexico.length-2].v2 = mexicoFixing[mexico.length-4].location;
   mexicoFixing[mexico.length-1].v1 = mexicoFixing[mexico.length-2].location;
   mexicoFixing[mexico.length-1].v2 = mexicoFixing[mexico.length-3].location;
+  //calcular las distancias
   return mexicoFixing.map((l) => {
       let {location, v1, v2} = l;
       let d1 = Math.sqrt( Math.pow( v1.lat - location.lat,2) + Math.pow(v1.long- location.long,2));
